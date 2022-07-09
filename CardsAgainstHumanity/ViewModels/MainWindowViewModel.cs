f40c6d34b99b147c45785938fa62f1c3dbeef2d7
DanBirks888 <danbirks888@gmail.com>
2022-07-09 16:00:27 +0100

    Working Application

--- CardsAgainstHumanity/ViewModels/MainWindowViewModel.cs
+++ CardsAgainstHumanity/ViewModels/MainWindowViewModel.cs
@@ -1,37 +1,27 @@
-﻿using System.IO;
-using System.Media;
-using Avalonia.Controls;
-using Avalonia.Data;
+﻿using System;
+using Avalonia;
+using Avalonia.Platform;
+using Avalonia.Shared.PlatformSupport;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.Processing;
-using Tmds.DBus;
 using Image = SixLabors.ImageSharp.Image;
 
 
 namespace CardsAgainstHumanity.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
-    {
-        public string Bakky => "transparent";
-        public string Title => "Cards Of Humanity";
-        public string Title2 => "Cards Of Humanity";
-        private static string btnClickPath = "CardsAgainstHumanity/Assets/MH - Item Found.mp3";
-        private static string cardsOfHumanityPath = "CardsAgainstHumanity/Assets/cardsAgainstHumanity.png";
-
-
+    { 
+        private static string cardsOfHumanityPath =  $"../../../Assets/cardsAgainstHumanity.png";
+        public string? Input { get; set; }
+        
         public void CreateCards()
         {
-            var player = new SoundPlayer(btnClickPath);
-        }
-
-        public static void CreateAllCards(string path)
-        {
-            var cardNames = File.ReadAllLines(path);
+            if (string.IsNullOrWhiteSpace(Input)) return;
+            var cardNames = Input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
             foreach (var cardName in cardNames) CreateCard(cardName);
         }
-
 
         public static void CreateCard(string text)
         {
@@ -47,7 +37,7 @@ namespace CardsAgainstHumanity.ViewModels
                 LineSpacing = 1
             };
             image.Mutate(m => m.DrawText(options, text, Color.Black));
-            image.SaveAsPng($"../../../../../Desktop/Test Cards/{GenerateName(text)}.png");
+            image.SaveAsPng($"../../../../../../Desktop/{GenerateName(text)}.png");
         }
 
         private static string GenerateName(string name)
